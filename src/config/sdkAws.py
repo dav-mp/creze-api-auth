fa8389036fd00476021ad7f9841f0f7efbd4d6d0
David Mayo <davidmayopacheco@gmail.com>
2024-11-07 15:22:03 -0600

    Cambios en Singleton sdkAws

--- src/config/sdkAws.py
+++ src/config/sdkAws.py
@@ -1,24 +1,21 @@
 import boto3
 
 class SdkAws:
-
     _instance = None
-
-    def getInstance():
-        if SdkAws._instance is None:
-            SdkAws
-            return SdkAws._instance
-
-        return SdkAws._instance
-        
 
     def __init__(self) -> None:
         if SdkAws._instance is not None:
-            return True
+            raise Exception("Esta clase es un singleton. Usa el método getInstance() para obtener la instancia.")
         else:
-            self.createInstance()
+            self.session = boto3.Session()  # Crear una sesión de boto3
+            SdkAws._instance = self
+            print("Sesión de Boto3 creada.", SdkAws._instance)
 
+    @classmethod
+    def getInstance(cls):
+        if cls._instance is None:
+            cls._instance = SdkAws()
+        return cls._instance
 
-    def createInstance():
-        SdkAws._instance = boto3
-        
+    def get_session(self):
+        return self.session
