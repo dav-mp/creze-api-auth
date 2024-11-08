b85e3b6ad245f1c1fb170538490f8153c7c4dfb7
David Mayo <davidmayopacheco@gmail.com>
2024-11-07 18:08:53 -0600

    Creacion de funcion para registar usuario en serviceProvider

--- src/service/authServiceProvider.py
+++ src/service/authServiceProvider.py
@@ -1,23 +1,37 @@
+# Importa SdkAws y EnvsAwsSm
 from src.config import SdkAws, EnvsAwsSm
+from src.domain.dto import UserRegisterDTO
+import json
 
 class AuthServiceProvider:
-
     sdk = None
+    envsCognito = None
 
     def __init__(self, sdkProvider: SdkAws) -> None:
-
         self.sdk = sdkProvider
-
-        pass
-
-
-    def getEnvsBySdk( self ):
-
         envs_aws_sm = EnvsAwsSm()  
-        envs = envs_aws_sm.getEnvs() 
+        self.envsCognito = envs_aws_sm.getEnvs() 
+    
+    def userRegister(self, user: UserRegisterDTO):
 
-        print('AYUDAAAAAAAA')
-        print(envs)
+        try:
+            envs = json.loads(self.envsCognito)
 
-        pass
-        # self.sdk
+            session = self.sdk.get_session()
+            cognitoClient = session.client('cognito-idp', region_name='us-east-1')
+
+            response = cognitoClient.sign_up(
+                ClientId=envs["CLIENT_ID"],
+                Username=user["email"], 
+                Password=user["password"],
+                UserAttributes=[
+                    {'Name': 'email', 'Value': user["email"]},
+                    {'Name': 'name', 'Value': user["name"]}  
+                ]
+            )
+
+            return response
+        except cognitoClient.exceptions.UsernameExistsException:
+            return {"error": "Email already exists"}, 400
+        except Exception as e:
+            return {"error": str(e)}, 500
