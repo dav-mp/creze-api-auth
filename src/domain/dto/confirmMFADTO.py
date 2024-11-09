f03b2533714f55c9c1d934d3db94961c434fe749
David Mayo <davidmayopacheco@gmail.com>
2024-11-09 11:59:59 -0600

    Creacion de confirmMFADTO

--- src/domain/dto/confirmMFADTO.py
+++ src/domain/dto/confirmMFADTO.py
@@ -3,9 +3,9 @@ from marshmallow import Schema, fields, validate, ValidationError
 
 class ConfirmMFADTO(Schema):
     session = fields.String(required=True, validate=validate.Length(min=1))
-    confirmationCode = fields.String(
+    userCode = fields.String(
         required=True, 
-        userCode=[validate.Length(equal=6), validate.Regexp(r'^\d{6}$')]  # Asegura que tenga 6 dígitos numéricos
+        validate=[validate.Length(equal=6), validate.Regexp(r'^\d{6}$')]  # Asegura que tenga 6 dígitos numéricos
     )
 
 
