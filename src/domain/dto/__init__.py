d56d8560e5d081f29a8cc07bfd0517e8fb4682e2
David Mayo <davidmayopacheco@gmail.com>
2024-11-09 07:54:27 -0600

    Creacion de ConfirmMFADTO

--- src/domain/dto/__init__.py
+++ src/domain/dto/__init__.py
@@ -1,9 +1,11 @@
 from src.domain.dto.userRegisterDTO import UserRegisterDTO
 from src.domain.dto.userConfirmDTO import UserConfirmDTO
 from src.domain.dto.userLoginDTO import UserLoginDTO
+from src.domain.dto.confirmMFADTO import ConfirmMFADTO
 
 __all__ = [
     "UserRegisterDTO",
     "UserConfirmDTO",
-    "UserLoginDTO"
+    "UserLoginDTO",
+    "ConfirmMFADTO"
 ]
